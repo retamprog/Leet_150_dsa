a9727fd5c8ff0489a8385196544fd917d09dc7f3
retamprog <retamphy2004@gmail.com>
2025-07-20 00:33:27 +0530

    initial commit

---
+++ src/remove_ele.java
@@ -0,0 +1,11 @@
+public class remove_ele{
+    public static void remove_element(int nums[],int val){
+          // > okay let say i hit the logic jackpot
+         
+
+    }
+    public static void main(String[] args) {
+        int nums[]={0,1,2,2,3,0,4,2};
+        remove_element(nums,2);
+    }
+}
