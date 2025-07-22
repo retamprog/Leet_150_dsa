ac8f23a2157924a9327437cdf30829b750cd49fe
retamprog <retamphy2004@gmail.com>
2025-07-23 01:23:57 +0530

    just finished the minsubArr prob , I hate it...

--- src/minSubArr.java
+++ src/minSubArr.java
@@ -2,26 +2,22 @@ public class minSubArr{
 public static int min_sub_arr(int nums[],int target){
    // using the sliding window approach
    int n=nums.length;
+   int left=0;
    int sum=0;
    int minLength=Integer.MAX_VALUE;
-   int i=0;int j=0;// here j is the right pointer and i is the left pointer
-   while(i<=j){
-     sum+=nums[j];
-     if(sum>=target && j<n){
-	      minLength=Math.min(minLength,j-i+1);
-         // System.out.println("Hello");
-         System.out.println("minLength---->"+minLength);
-         sum-=nums[i];
-         i+=1;	
-     }
-     else  
-        j++; 
+   for(int right=0;right<n;right++){
+       sum+=nums[right];
+       while(sum>=target){
+          minLength=Math.min(minLength,right-left+1);
+          sum-=nums[left];
+          left++;
+       }
    }
-    return minLength;
-}	
-public static void main(String args[]){
-    int nums[]={2,3,1,2,4,3};  // ans : 2
-    System.out.println(min_sub_arr(nums,7));
+   return minLength;
+}
+   public static void main(String args[]){
+    int nums[]={1,4,4};  // ans : 2
+    System.out.println(min_sub_arr(nums,4));
 }
 }
 
