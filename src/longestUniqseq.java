b4ce6518798cdf75cddc66d8757a5b5a76c6d1a1
retamprog <retamphy2004@gmail.com>
2025-07-24 18:44:34 +0530

    fifth commit

---
+++ src/longestUniqseq.java
@@ -0,0 +1,16 @@
+public class longestUniqseq{
+    public static int longest_uniq_sequence(String test){
+        int maxLength=0;
+        int left=0;
+        int n=test.length();
+        int [] count=new int[128];
+        for(int right=0;right<n;right++){
+           
+        }
+    }
+
+    public static void main(String args[]){
+        String test1="abcabcbb";
+        String test2="pwwkew";
+    }
+}
